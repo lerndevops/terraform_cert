787d35d361f737dc5937e242f578f9da7cb7d043
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-26 22:33:05 +0530

    Create 2.output.tf

---
+++ Day2/2.output.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "aws-multi-instance" {
+  count         = 2
+  ami           = "ami-0747bdcabd34c712a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "demoinstnce-${count.index}"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.aws-multi-instance[*].public_ip
+}
+
+#it creates aws instances and prints ips.
