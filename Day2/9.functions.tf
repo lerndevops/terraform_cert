da135825cbd69718a36ebe7210eade36b427fc5e
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-28 01:47:11 +0530

    Create 9.functions.tf

---
+++ Day2/9.functions.tf
@@ -0,0 +1,12 @@
+
+
+
+# The Terraform configuration language allows you to write declarative expressions to create infrastructure. While the configuration language is not a programming language, you can use several built-in functions to perform operations dynamically and effectively.
+
+# NO scope for user defined functions, only bult-in.
+
+# terraform console helps in testing functions
+
+# https://learn.hashicorp.com/tutorials/terraform/functions
+
+
