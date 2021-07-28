9601e50f9714418f233b686bb9871bd9e99daf20
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-28 15:46:40 +0530

    Create 12.graph.tf

---
+++ Day2/12.graph.tf
@@ -0,0 +1,12 @@
+
+
+# The terraform graph command is used to generate a visual representation of either a configuration or execution plan. The output is in the DOT format, which can be used by GraphViz to generate charts.
+
+# Outputs the visual execution graph of Terraform resources according to either the current configuration or an execution plan.
+
+# The graph is outputted in DOT format. The typical program that can read this format is GraphViz, but many web services are also available to read this format.
+
+# apt-get update
+# apt-get install graphviz
+
+
