ccb8ba5b428e4edf22fb3f73ca848154862773e3
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-28 15:58:19 +0530

    Update 12.graph.tf

--- Day2/12.graph.tf
+++ Day2/12.graph.tf
@@ -1,4 +1,25 @@
+provider "aws" {
+  region     = "us-east-1"
+ # access_key = "YOUR-ACCESS-KEY"
+ # secret_key = "YOUR-SECRET-KEY"
+}
 
+data "aws_ami" "test_ami" {
+  most_recent = true
+  owners = ["amazon"]
+# owners = ["self"] if we use OWN org account
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "instance-1" {
+    ami = data.aws_ami.test_ami.id
+   instance_type = "t2.micro"
+}
 
 # The terraform graph command is used to generate a visual representation of either a configuration or execution plan. The output is in the DOT format, which can be used by GraphViz to generate charts.
 
@@ -8,5 +29,8 @@
 
 # apt-get update
 # apt-get install graphviz
+# terraform graph > g.dot
+# cat g.dot |dot -Tsvg > gra.svg
+# copy the svg file and open with local browser
 
 
