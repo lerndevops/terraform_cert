aed875655c9fd2ed99a7fc8dbcf4e2c12091b2c8
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-29 18:17:43 +0530

    Create 2.module.tf

---
+++ Day3/Modules/2.module.tf
@@ -0,0 +1,24 @@
+## create a folder structure
+# mkdir modules work
+# cd modules;mkdir ec2;cd ec2 ;create ec2.tf as below
+
+provider "aws" {
+#  access_key = "accesskey_here"
+#  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "moduetest" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+}
+
+
+# NOW go to work folder nd create test.tf
+
+module "ec2module" {
+  source = "../modules/ec2"
+}
+  
+  #terraform init
+  # terraform plan
