0eb1035c095a45b8e4ae744f10a0e2e16d2f8fd5
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-29 14:33:00 +0530

    Create 1.ec2.tf

---
+++ Day3/state/1.ec2.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+#  access_key = "accesskey_here"
+#  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+  key_name = "terra"
+}
+
+# terraform apply
+# check state file terraform.tfstate
+# update the instance type, terraform plan --> you will see updated plan 
+# terraform apply will corect the instancetype as per config
+
+# now try changing sf inbound rules and test.
+
+
+# https://www.terraform.io/docs/language/state/index.html
