bda0410afbf7ff6744b94af57c8996d7c3cfa8a9
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-07-31 16:30:45 +0530

    Create 4.module_git.tf

---
+++ Day3/Modules/4.module_git.tf
@@ -0,0 +1,18 @@
+# This is the Terraform Code in local
+
+module "gitmodule" {
+  source = "github.com/lerndevops/test-repo"
+}
+# Create a github repo with the name test-repo, put module souce code over there. 
+  
+  
+# ec2.tf in git repo
+  
+  provider aws {
+    region = "us-east-1"
+  }
+  
+ resource "aws_instance" "test" {
+  ami           = "ami-05692172625678b4e"
+  instance_type = "t2.micro"
+}
