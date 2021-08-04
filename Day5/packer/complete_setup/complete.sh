8a5dd3b19aeaeb263393e1932822723127ee51d3
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-08-04 22:45:31 +0530

    Update complete.sh

--- Day5/packer/complete_setup/complete.sh
+++ Day5/packer/complete_setup/complete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo " Building the AMI "
-packer build ami.json > result
+packer build packer_ami.json > result
 export TF_VAR_amiid=$(tail -2 result | awk  '{print $2}')
 echo " The AMI created and id is  $TF_VAR_amiid "
 echo " Creating EC2 instance"
