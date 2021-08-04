753e5d512cc4afe48046d522d7a33ce4238b0ca6
lerndevops <45846027+lerndevops@users.noreply.github.com>
2021-08-04 23:33:25 +0530

    Update sensetive.tf

--- Day5/sensetive.tf
+++ Day5/sensetive.tf
@@ -1,1 +1,14 @@
+resource "aws_instance" "aws-multi-instance" {
+  count         = 1
+  ami           = "ami-0747bdcabd34c712a"
+  instance_type = "t2.micro"
 
+  tags = {
+    Name = "demoinstnce-${count.index}"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.aws-multi-instance[*].public_ip
+  sensitive = true ## This will not show the details in output
+}
